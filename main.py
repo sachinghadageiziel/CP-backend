db521e61ea6e45e624147bac64b7f4b30ee7202a
Sachin Ghadage <sachin.ghadage@izielhealthcare.com>
2025-09-17 17:13:18 +0530

    Initial commit3 - FastAPI backend Docker

--- main.py
+++ main.py
@@ -19,7 +19,11 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://gilded-gaufre-2eadbb.netlify.app/",
         "https://cp-backend-6n14.onrender.com"
+
+
+
     ],  # React dev server
     allow_credentials=True,
     allow_methods=["*"],
